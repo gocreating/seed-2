8575b8009b3af39ba0b20445034a2a48d12e56a8
gocreating <gocreating@gmail.com>
2015-08-30 13:50:45 +0800

    colorful request log

--- src/core/handlers/environmentHandler.js
+++ src/core/handlers/environmentHandler.js
@@ -24,13 +24,39 @@ export default (app) => {
   /**
    * logger
    */
+  morgan.token('colorStatus', (req, res) => {
+    const status = res.statusCode;
+    let color = '';
+
+    if (status < 200) {
+      // 1xx
+      color = '\x1b[0m';
+    } else if (status < 300) {
+      // 2xx
+      color = '\x1b[0;32m';
+    } else if (status < 400) {
+      // 3xx
+      color = '\x1b[1;33m';
+    } else if (status < 500) {
+      // 4xx
+      color = '\x1b[0;31m';
+    } else {
+      // 5xx
+      color = '\x1b[0;35m';
+    }
+
+    return color + status + '\x1b[0m';
+  });
+
   app.use(morgan(
-    ':remote-addr\t' +
-    ':status ' +
-    ':method ' +
-    ':url\t' +
-    ':res[content-length] - ' +
-    ':response-time ms'
+    '\x1b[1;30m' + '[:date[iso]] ' +
+    '\x1b[0m'    + ':remote-addr\t' +
+                   ':colorStatus ' +
+                   ':method ' +
+                   ':url\t' +
+    '\x1b[0m'    + ':res[content-length] - ' +
+    '\x1b[0;36m' + ':response-time ms' +
+    '\x1b[0m'
   ));
   // @endif
 
