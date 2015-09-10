77e4691eb56bbc6218fc2a9ea54cbf8e9b04a763
gocreating <gocreating@gmail.com>
2015-09-10 16:22:05 +0800

    adjust client-side routing

--- src/core/routes.js
+++ src/core/routes.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import {Location, Locations} from 'react-router-component';
+import CaptureClicks from 'react-router-component/lib/CaptureClicks';
 
 const pathPrefix = require('./settings.common').installedApps.core.pathPrefix;
 
 export default class App extends React.Component {
   render() {
-    return <Locations path={this.props.path}>
-      <Location
-        path={`${pathPrefix}/`}
-        handler={require('./flux/views/pages/HomePage.jsx')} />
-      <Location
-        path={`${pathPrefix}/about`}
-        handler={require('./flux/views/pages/AboutPage.jsx')} />
-    </Locations>;
+    return <CaptureClicks>
+      <Locations path={this.props.path}>
+        <Location
+          path={`${pathPrefix}/`}
+          handler={require('./flux/views/pages/HomePage.jsx')} />
+        <Location
+          path={`${pathPrefix}/about`}
+          handler={require('./flux/views/pages/AboutPage.jsx')} />
+      </Locations>
+    </CaptureClicks>;
   }
 };
--- src/todo/routes.js
+++ src/todo/routes.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {Location, Locations} from 'react-router-component';
+import CaptureClicks from 'react-router-component/lib/CaptureClicks';
 
 const pathPrefix = require('../core/settings.common').installedApps.todo.pathPrefix;
 
 export default class App extends React.Component {
   render() {
-    return <Locations path={this.props.path}>
-      <Location
-        path={`${pathPrefix}`}
-        handler={require('./flux/views/pages/TodoPage.jsx')} />
-    </Locations>;
+    return <CaptureClicks>
+      <Locations path={this.props.path}>
+        <Location
+          path={`${pathPrefix}`}
+          handler={require('./flux/views/pages/TodoPage.jsx')} />
+      </Locations>
+    </CaptureClicks>;
   }
 };
